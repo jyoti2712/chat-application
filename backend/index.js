aec5ff9f884a2a5b0ff15df2e3a38b6ecd463250
Jyoti2712 <pandajyoti2000@gmail.com>
2025-01-04 22:34:20 +0530

    chnages

--- backend/index.js
+++ backend/index.js
@@ -28,12 +28,13 @@ app.use("/api/messages", messageRoutes);
 
 
 
-if(process.env.NODE_ENV === "production"){
+if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
-    app.get("*", (req,res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-    })
-}
+  
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+  }
 
 server.listen(3001, () => {
     console.log("Server is running on port " + PORT);
